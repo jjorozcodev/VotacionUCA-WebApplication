73b94a7cbea4c0d72a7e7124b60ae7327863a176
Francela Perez <francelaperez24@gmail.com>
2019-08-09 02:39:59 -0600

    Integración del Sweetalert
    En las vistas Gestión (Eliminar registro) y Votar

--- VotacionUCAWebApplication/Content/js/script.js
+++ VotacionUCAWebApplication/Content/js/script.js
@@ -1,1 +1,31 @@
-﻿
+﻿function Advertencia() {
+    Swal.fire({
+        title: '¿Estás seguro que deseas eliminar este registro?',
+        type: 'warning',
+        showCancelButton: true,
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'Eliminado!',
+                'El registro se eliminó con éxito',
+                'success'
+            )
+        }
+    })
+}
+
+function Acceso() {
+    var Usuario = $('#Usuario').val();
+    var Clave = $('#Clave').val();
+    var TipoUsuario = $('#TipoUsuario').val();
+    
+    debugger;
+
+    $.post('/ajax/acceso', Usuario).done(function () {
+        location.href = '/inicio/votaciones';
+    }).fail();
+}
