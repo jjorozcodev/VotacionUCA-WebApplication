0c8672d5c6116d96e104e4d7f007aec4e670de8e
Francela Perez <francelaperez24@gmail.com>
2019-08-11 22:30:15 -0600

    Obtener listado de las votaciones
    Se creó un método en el script llamado ListarVotaciones() y se cargan los datos en la vista Gestión

--- VotacionUCAWebApplication/Content/js/script.js
+++ VotacionUCAWebApplication/Content/js/script.js
@@ -24,7 +24,6 @@ function Acceso() {
     var tipousuario = $('#TipoUsuario').is(":checked");
 
     var data = { Usuario: usuario, TipoUsuario: tipousuario };
-
     // POST usando AJAX y JQuery
     $.post('/ajax/acceso', data).done(function (resp) {
         if (resp) {
@@ -44,3 +43,37 @@ function UsuarioActual() {
         $('#usuarioActual').html('' + data);
     });
 }
+
+function ListarVotaciones() {
+
+    // GET usando AJAX y JQuery
+    $.get('/Ajax/ListarVotaciones').done(function (data) {
+        debugger;
+        $.each(data, function () {
+
+            var estado = "";
+            if (this.Abierto === true) {
+                estado = "Abierto";
+            }
+            else {
+                estado = "Cerrado";
+            }
+
+            $("#datosTabla").append(
+                '<tr>'
+                + '<td>' + this.Id + '</td>'
+                + '<td>' + this.Descripcion + '</td>'
+                + '<td>' + this.CodGrupo + '</td>'
+                + '<td>' + estado + '</td>'
+                + '<td>'
+                + '<a href= "../Votaciones/Candidatos/' + this.Id + '" class="btn-sm btn-warning"><i class="lni-eye" style="color:white"></i></a>'
+                + '<a href="../Votaciones/Editar/' + this.Id + '" class="btn-sm btn-primary"><i class="lni-pencil"></i></a>'
+                + '<a href="#" onclick="Advertencia()" class="btn-sm btn-danger"><i class="lni-trash"></i></a>'
+                + '</td>'
+                + '<tr>'
+                
+            );
+        });
+    }).fail();
+}
+
