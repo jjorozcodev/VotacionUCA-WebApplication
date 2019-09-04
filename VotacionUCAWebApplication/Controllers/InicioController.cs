dc10d78bd6bca4892fef41f47bf4b13d5f0e0764
JJ <jj.orozcolopez.13@gmail.com>
2019-09-04 16:56:13 -0600

    Correcciones en prototipo.
    Actualización de jQuery 3.4.1, correcciones en pantallas, centrando texto de título, url de cierre de sesión.

--- VotacionUCAWebApplication/Controllers/InicioController.cs
+++ VotacionUCAWebApplication/Controllers/InicioController.cs
@@ -6,6 +6,17 @@ namespace VotacionUCAWebApplication.Controllers
     {
         public ActionResult Acceso()
         {
+            if (!(Session["idUsuarioActual"] ==  null))
+            {
+                if (Session["usuarioActual"].ToString().Equals("admin"))
+                {
+                    RedirectToAction("Gestion");
+                }
+                else
+                {
+                    RedirectToAction("Votaciones");
+                }
+            }
             return View();
         }
         public ActionResult Gestion()
